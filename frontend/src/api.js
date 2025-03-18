2df324200d3dea499fe10d06acd5ff73953e4dc8
kevin68107 <kevin.aihoon@amalitech.com>
2025-03-18 13:20:10 +0000

    added a url path to the api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { ACCESS_TOKEN } from './constants';
 
+const apiUrl = "https://c51f5191-5861-4dc1-954c-07c67b5032b5-dev.e1-us-east-azure.choreoapis.dev/djangodeploy/backend/v1"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 api.interceptors.request.use(
